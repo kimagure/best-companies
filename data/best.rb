57f53272a0eabcfe6432af1bdb3846c403b3c2ab
shido <jrullmann@gmail.com>
2015-01-01 18:24:42 -0500

    Corrected things added/removed for debugging.

--- data/best.rb
+++ data/best.rb
@@ -19,46 +19,46 @@ for i in 2..10 do
 end
 puts "Popular DCMA users total: #{popular_va.total_count} Fetched: #{popular_va_items.length}"
 
-# mongo_users = db['users']
-# popular_va_items.each {|item|
-#   user = item.rels[:self].get.data
+mongo_users = db['users']
+popular_va_items.each {|item|
+  user = item.rels[:self].get.data
 
-#   repos = item.rels[:repos].get.data
-#   languages = [].to_set
+  repos = item.rels[:repos].get.data
+  languages = [].to_set
 
-#   repos.each {|repo|
-#     languages = languages.merge(repo.rels[:languages].get.data.fields)
-#   }
+  repos.each {|repo|
+    languages = languages.merge(repo.rels[:languages].get.data.fields)
+  }
 
-#   doc = {
-#     "login" => user.login,
-#     "id" => user.id,
-#     "avatar_url" => user.rels[:avatar].href,
-#     "gravatar_id" => user.gravatar_id,
-#     "type" => user.type,
-#     "site_admin" => user.site_admin,
-#     "name" => user.name,
-#     "company" => user.company,
-#     "blog" => user.blog,
-#     "location" => user.location,
-#     "email" => user.email,
-#     "hirable" => user.hirable,
-#     "bio" => user.bio,
-#     "public_repos" => user.public_repos,
-#     "followers" => user.followers,
-#     "following" => user.following,
-#     "created_at" => user.created_at,
-#     "updated_at" => user.updated_at,
-#     "public_gists" => user.public_gists,
-#     "languages" => languages.to_a()
-#   }
+  doc = {
+    "login" => user.login,
+    "id" => user.id,
+    "avatar_url" => user.rels[:avatar].href,
+    "gravatar_id" => user.gravatar_id,
+    "type" => user.type,
+    "site_admin" => user.site_admin,
+    "name" => user.name,
+    "company" => user.company,
+    "blog" => user.blog,
+    "location" => user.location,
+    "email" => user.email,
+    "hirable" => user.hirable,
+    "bio" => user.bio,
+    "public_repos" => user.public_repos,
+    "followers" => user.followers,
+    "following" => user.following,
+    "created_at" => user.created_at,
+    "updated_at" => user.updated_at,
+    "public_gists" => user.public_gists,
+    "languages" => languages.to_a()
+  }
 
-#   result = mongo_users.update(
-#     { id: user.id },
-#     { '$set' => doc },
-#     { upsert: true }
-#   )
-# }
+  result = mongo_users.update(
+    { id: user.id },
+    { '$set' => doc },
+    { upsert: true }
+  )
+}
 
 companies = db.collection("users").aggregate([
   {"$match" => {company: {"$ne" => nil}}},
@@ -100,8 +100,6 @@ companies = db.collection("users").aggregate([
   {"$sort" => {users_count: -1}}
 ]).find
 
-puts companies.count()
-
 companies.each {|company|
   company["name"] = company["_id"]
   company.delete("_id")
@@ -112,5 +110,3 @@ companies.each {|company|
     { upsert: true }
   )
 }
-
-sleep 5
